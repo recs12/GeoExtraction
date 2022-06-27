4ba29413ad97087e385e25709239594f10daec3f
Slimane Rechdi <recs@premiertech.com>
2022-06-27 20:40:22 +0200

    in progress

--- Program.fs
+++ Program.fs
@@ -1,8 +1,18 @@
-﻿open System
+﻿(*
+    Commande line standalone executable.
+    Generate csv with following columns:
+    Look for *.GEO files
+    FolderPath, Filename,TotalNumberEngravings, Engraving, LineNumber, Text, OverwritingText
+*)
+
+open System
 open System.IO
 
+// cmd will prompt for root path
+let csv = @"C:\Users\recs\OneDrive - Premier Tech\Bureau\GEO\MGM TEST FILES\GEO\engraves.csv"
 
-let Glob (folder:string) =
+
+let globglob (folder:string) =
     let files = Directory.GetFileSystemEntries(folder, "*.GEO", SearchOption.AllDirectories)
     files
     
@@ -10,6 +20,17 @@ let Glob (folder:string) =
 let FindIndex arr elem = 
     arr 
     |> Array.tryFindIndex ((=) elem)
+    
+
+let countNumberOfTxt (listOfWords : array<string>) :int =
+    let result = Array.filter (fun x -> x = "TXT") listOfWords
+    result.Length
+
+let countNumberOfTxtAndIndex (listOfWords : array<string>) :array<int*string> =
+    let result = Array.filter (fun x -> x = "TXT") listOfWords
+    let _end:int = result.Length - 1
+    let txtsIndex = [|0 .. _end|]
+    Array.zip txtsIndex result
     
 
 let LookUpForTXT (collection:string[]) (idx:option<int>) :string =
@@ -20,9 +41,11 @@ let LookUpForTXT (collection:string[]) (idx:option<int>) :string =
 
 let GetFileGravure geoPath=
     let CollectionElements = File.ReadAllLines(geoPath)
-    // let NumberOfGravure = Array.countBy CollectionElements "TXT" 
+    let numberOfInstanceOfTxt: int =  countNumberOfTxt CollectionElements
+    
     let indexWhereTXT = FindIndex CollectionElements "TXT"
     let gravure = LookUpForTXT CollectionElements indexWhereTXT
+
     (gravure, indexWhereTXT)
     // (total, {(gravure, line);(gravure, line);(gravure, line);})
     
@@ -38,21 +61,26 @@ let appendCSV (_path:string) folderPath filename totalNumberOfEngravings engravi
     file.WriteLine($"{folderPath},{filename},{totalNumberOfEngravings},{engraving},{engravingLineNumber},{engravingText},{overwritingText},")
 
 
-let csv = @"C:\Users\recs\OneDrive - Premier Tech\Bureau\GEO\MGM TEST FILES\GEO\engraves.csv"
 
 
 [<EntryPoint>]
 let main args =
     createCSV  csv|>ignore
-    for _file in (Glob args[0]) do
+    for _file in (globglob args[0]) do
+
         let fileName_ = Path.GetFileName(_file) 
         let directoryName_ = Path.GetDirectoryName(_file) 
+
         let (grave, num) = GetFileGravure _file
-     
         let numValue = 
             match num with
             | Some(num) -> num.ToString()
             | None -> ""
+        
+        // match numberofTxt with 
+        // | 1 -> appendCSV
+        // | _ -> loop appendCSV
+        
         appendCSV csv directoryName_ fileName_ "?" "?"  numValue  grave  |> ignore
     0
 
@@ -62,6 +90,8 @@ let main args =
 // todo : check for number of TXT in file.
 // todo : think of how to loop to the number of number of TXT
 // todo : line of the gravure minus 1
+// todo : look for gravure with index number of number
+// exemple:: give index -> get content
    
 
 
